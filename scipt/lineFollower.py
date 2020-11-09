cf41d390468e974d7779d45d1cc53d09affd637f
haoweilou <591193117@qq.com>
2020-11-08 20:33:21 -0800

    version works for green

--- scipt/lineFollower.py
+++ scipt/lineFollower.py
@@ -83,7 +83,7 @@ class Follower:
   def __init__(self):
     self.bridge = cv_bridge.CvBridge()
     #cv2.namedWindow("window", 1)
-    self.image_sub = rospy.Subscriber('camera/rgb/image_raw',
+    self.image_sub = rospy.Subscriber('camera/image',
                                       Image, self.image_callback)
     self.cmd_vel_pub = rospy.Publisher('cmd_vel',
                                        Twist, queue_size=1)
@@ -97,7 +97,7 @@ class Follower:
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     #randomly chosen values
-    lower_yellow = numpy.array([0,  0,  0])
+    lower_yellow = numpy.array([0,  100,  0])
     upper_yellow = numpy.array([255, 255, 250])
     #need to find the lowest and highest threshold for white lines
     #opencv rgb value
@@ -126,12 +126,12 @@ class Follower:
       cy = int(M['m01']/M['m00'])
       cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
       err = cx - w/2
-      self.twist.linear.x = 0.08
+      self.twist.linear.x = 0.1
       self.twist.angular.z = -float(err) / 100
       #print(-float(err) / 60)
       print("Hello")
       self.cmd_vel_pub.publish(self.twist)
-      print("I completed the publish?")
+      print(self.twist)
     #print('HAHA')
     #center, h = stop_sign_detection(image)
     #cv2.circle(image, center, 20, (0,255,0), -1)
